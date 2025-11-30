5f95d460ec2fe3e95a5de782eb5108e678df10c0
mona lisa <williamkubai101@gmail.com>
2025-11-30 09:20:59 +0300

    Javascript object basics

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,14 @@
+module.exports = class Rectangle {
+    constructor(w, h) {
+        if (w && h > 0) {
+            this.width = w;
+            this.height = h;
+        }
+    }
+    
+    print() {
+        for (let i=0; i < this.height; i++) {
+            console.log("x".repeat(this.width));
+        }
+    }
+}
